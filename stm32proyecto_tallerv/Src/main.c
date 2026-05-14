8e54982e646f2c5be52331d396b6bf5b68e64e5b
Juan Jose Giraldo Arbelaez <jgiraldoar@unal.edu.co>
2026-05-14 11:22:03 -0500

    Recreate project and modificate .gitignore

--- stm32proyecto_tallerv/Src/main.c
+++ stm32proyecto_tallerv/Src/main.c
@@ -18,12 +18,63 @@
 
 #include <stdint.h>
 
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+
+//Definicion de variables del sistema
+uint8_t var_a = 0;
+uint16_t var_b = 0;
+uint32_t var_c = 0;
+
+uint8_t overflow_demo  = 0;
+
+
+uint16_t var_a_dec = 0;
+uint16_t var_b_bin = 0;
+uint16_t var_c_hex = 0;
+
+
+
+
 int main(void)
 {
+
+	var_a = 100;
+	var_b = 4968;
+	var_c = 12345678;
+
+	var_a_dec = 32;
+	var_b_bin = 0b100000;
+	var_c_hex = 0x20;
+
+	//cargando el valor xxx en la variable yyy
+	var_b_bin = var_b_bin << 3; //prediccion: 0b10000000 = 256
+	var_b_bin = var_b_bin >> 3; //prediccion: 0b100 = 4 :c
+
+	//exponiendo el caso de un overflow
+	var_a = 255;
+	var_b = 255;
+	var_c = 255;
+
+	//incremento el valor de la variable 8bit en 1 y lo cargo en la variable overflow_demo
+	overflow_demo = var_a + 1;
+	overflow_demo = overflow_demo + 1;
+
+	overflow_demo = 735;
+	overflow_demo = 0;
+
+	for(uint16_t counter = 0; counter < 735; counter ++){
+		overflow_demo++;
+	}
+
     /* Loop forever */
-	for(;;);
+	while(1){
+
+
+	}
+
+	return 0;
 }
